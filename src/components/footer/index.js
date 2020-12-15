ab612251f76cc423143c2e5b434804163828483e
mhdhassan99 <mhassan.work99@gmail.com>
2020-12-15 17:48:45 -0500

    still on home page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Column, Link, Title, Text, Break } from './styles/footer';
 
 const Footer = ({ children, ...restProps }) => {
     return (
@@ -11,16 +12,24 @@ export default Footer;
 
 Footer.Row = function FooterRow({ children, ...restProps }) {
     return <Row {...restProps}> {children} </Row>
-} 
+}; 
 
 Footer.Column = function FooterColumn({ children, ...restProps }) {
     return <Column {...restProps}> {children} </Column>
-} 
+}; 
 
 Footer.Link = function FooterLink({ children, ...restProps }) {
     return <Link {...restProps}> {children} </Link>
-} 
+}; 
 
 Footer.Title = function FooterTitle({ children, ...restProps }) {
     return <Title {...restProps}> {children} </Title>
-} 
+}; 
+
+Footer.Text = function FooterText({ children, ...restProps }) {
+    return <Text {...restProps}> {children} </Text>
+}; 
+
+Footer.Break = function FooterBreak({ children, ...restProps }) {
+    return <Break {...restProps}> {children} </Break>
+}; 
---
+++ src/containers/footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Footer } from '../components'
+
+export function FooterContainer() {
+    return (
+        <Footer>
+            <Footer.Title>Questions? Contact us.</Footer.Title>
+            <Footer.Break />
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link herf="#">FAQ</Footer.Link>
+                    <Footer.Link herf="#">Investor Relations</Footer.Link>
+                    <Footer.Link herf="#">Ways to watch</Footer.Link>
+                    <Footer.Link herf="#">Corporate Information</Footer.Link>
+                    <Footer.Link herf="#">Netflix Originals</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link herf="#">Help center</Footer.Link>
+                    <Footer.Link herf="#">Terms of use</Footer.Link>
+                    <Footer.Link herf="#">Jobs</Footer.Link>
+                    <Footer.Link herf="#">Contact us</Footer.Link>
+                </Footer.Column>
+
+                <Footer.Column>
+                    <Footer.Link herf="#">Account</Footer.Link>
+                    <Footer.Link herf="#">Redeem Gift Cards</Footer.Link>
+                    <Footer.Link herf="#">Privacy</Footer.Link>
+                    <Footer.Link herf="#">Speed Test</Footer.Link>
+                </Footer.Column>
+
+            </Footer.Row>
+        </Footer>
+    );
+}
+
+
