748a202e70ff5b33f399f0ef5fe352b564677bc4
mhdhassan99 <mhassan.work99@gmail.com>
2020-12-23 12:50:00 -0500

    last thing on home page

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json';
 
 export function FaqsContainer() {
@@ -12,7 +12,15 @@ export function FaqsContainer() {
                     <Accordion.Body>{item.body}</Accordion.Body>
                 </Accordion.Item>
             ))}
-            
+            <Accordion.Item />
+
+            <OptForm>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button> Try it Now </OptForm.Button>
+                <OptForm.Text> 
+                    Ready to watch? Enter Your Email to create or restart your membership  
+                </OptForm.Text>
+            </OptForm>
         </Accordion>
     );
 };
