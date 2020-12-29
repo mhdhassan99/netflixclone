e6e460bec96dcd63c36b1f149858fa1ca725cb7e
mhdhassan99 <mhassan.work99@gmail.com>
2020-12-29 10:37:27 -0500

    working on routs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
-    <>
-     <JumbotronContainer />
-     <FaqsContainer />
-     <FooterContainer />
-    </> 
+    <Router>
+      <Route exact path="/user">
+        <p>Hello I am a user</p>
+      </Route>
+
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router> 
   )   
 }
 
