5d3c2ec283cd558bb5f471cc424f36bde96dfe1e
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-04 21:52:09 -0500

    setting up firebase

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+
+
+const config = {};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
