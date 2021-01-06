649bea55fd479eab90972264e213986113666585
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-06 16:55:27 -0500

    working on signin

---
+++ src/components/form/styles/form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+export const Container = styled.div`
+
+`;
+
+export const Error = styled.div`
+
+`;
+
+export const Base = styled.div`
+
+`;
+
