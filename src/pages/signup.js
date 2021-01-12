d387f8e50b60ecb0542656fd32cf15da1ebbb745
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-11 22:38:19 -0500

    sign up page compelete

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -20,38 +20,67 @@ export default function Signup() {
     const handleSignup = (e) => {
         e.preventDefault();
 
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then((result) => 
+                result.user
+                    .updateProfile({
+                        displayName: firstName,
+                        photoURL: Math.floor(Math.random() * 5) + 1,
+                    })
+                    .then(() => {
+                        history.push(ROUTES.BROWSE);
+                    })
+            )
+            .catch((error) => {
+                setFirstName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            })
     };
 
     return (
-        <HeaderContainer>
-            <Form>
-                <Form.Title>Sign up</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign up</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
 
-                <Form.Base onSubmit={handleSignup} method="POST">
-                    <Form.Input
-                        placeholder="First name"
-                        value={firstName}
-                        onChange={({ target }) => setFirstName(target.value)}
-                    />
-                    <Form.Input 
-                        placeholder="Email address"
-                        value={emailAddress}
-                        onChange={({ target }) => setEmailAddress(target.value)}
-                    />
-                    <Form.Input
-                        placeholder="Password"
-                        type="password"
-                        value={password}
-                        autoComplete="off"
-                        onChange={({ target }) => setPassword(target.value)}
-                    />
-                    <Form.Submit disabled={isInvalid} type="submit">
-                        Sign Up
-                    </Form.Submit>
-                </Form.Base>
+                    <Form.Base onSubmit={handleSignup} method="POST">
+                        <Form.Input
+                            placeholder="First name"
+                            value={firstName}
+                            onChange={({ target }) => setFirstName(target.value)}
+                        />
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            autoComplete="off"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit">
+                            Sign Up
+                        </Form.Submit>
 
-            </Form>
-        </HeaderContainer>
+                        <Form.Text>
+                            Already a User? <Form.Link to="/signin">Sign In Now</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn More. 
+                        </Form.TextSmall>
+                    </Form.Base>
+
+                </Form>
+            </HeaderContainer>
+            <FooterContainer/>
+        </>
     )
 }
