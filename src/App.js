d9ac09d40d770f17b687d3cbc1f86387af4b5ff5
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-12 14:41:30 -0500

    working on logged in user can see it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, Browse, Signup, Signin } from './pages'
+import { Home, Browse, Signup, Signin } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 function App() {
+
+  const user = {};
+
   return (
     <Router>
       <Route exact path="/browse">
         <Browse />
       </Route>
 
-      <Route exact path="/signup">
+      <IsUserRedirect 
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
+      </IsUserRedirect>
 
-      <Route exact path="/signin">
+      <IsUserRedirect 
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
+      </IsUserRedirect>
 
       <Route exact path={ROUTES.HOME}>
         <Home />
