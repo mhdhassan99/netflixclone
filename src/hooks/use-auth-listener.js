41efcce576c64a5829a3f3034a43f439c5080b4a
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-16 15:43:25 -0500

    created cutom hooks

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from 'react';
 import { FirebaseContext } from '../context/firebase';
 
-export default function userAuthListener() {
+export default function useAuthListener() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('authUser')));
 
     const { firebase } = useContext(FirebaseContext);
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+
+    const { series } = useContent('series')
+    console.log(series)
     return (
         <p>Hello browse</p>
     )
