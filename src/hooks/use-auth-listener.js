8586c06e44eb71032c1fa2d45f643393dec600e7
mhdhassan99 <mhassan.work99@gmail.com>
2021-01-16 14:59:58 -0500

    working with auth listner

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -7,4 +7,18 @@ export default function userAuthListener() {
     const { firebase } = useContext(FirebaseContext);
 
     
+    useEffect(() =>  {
+        const listener = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                localStorage.setItem('authUser', JSON.stringify(authUser));
+                setUser(authUser);
+            }else {
+                localStorage.removeItem('authUser');
+                setUser(null);
+            }
+        });
+        return () => listener();
+    }, []);
+
+    return { user };
 };
