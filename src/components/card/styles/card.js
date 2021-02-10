7538584f2def358029d7380cdfc17e322ae78143
mhdhassan99 <mhassan.work99@gmail.com>
2021-02-09 21:35:36 -0500

    more styling

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components/macro';
 
 export const Title = styled.p`
-
+    font-size: 24px;
+    color: #e5e5e5;
+    font-weight: bold;
+    margin-left: 56px;
+    margin-right: 56px;
+    margin-top: 0;
 `;
 
 export const Container = styled.div`
@@ -47,7 +52,7 @@ export const Feature = styled.div`
 `;
 
 export const FeatureTitle = styled(Title)`
-
+    margin-left: 0;
 `;
 
 export const FeatureText = styled.p`
@@ -55,11 +60,32 @@ export const FeatureText = styled.p`
 `;
 
 export const FeatureClose = styled.button`
+    color: white;
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    cursor: pointer;
+    background-color: transparent;
+    border: 0;
 
+    img (
+        filter: brightness(0) invert(1)
+        width: 24px;
+    )
 `;
 
 export const Maturity = styled.div`
-
+    background-color: ${({ rating }) => (rating >= 15 ? 'red' : 'green')};
+    border-radius: 15px;
+    width: 20px;
+    padding: 5px;
+    text-align: center;
+    color: white;
+    font-weight: bold;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    margin-right: 10px;
+    font-size: 12px;
+    
 `;
 
 export const Content = styled.div`
@@ -67,11 +93,16 @@ export const Content = styled.div`
 `;
 
 export const Meta = styled.div`
-
+    display: none;
+    position: absolute;
+    bottom: 0;
+    padding: 10px;
+    background-color: #00000008f;
 `;
 
 export const Entities = styled.div`
-
+    display: flex;
+    flex-direction: row;
 `;
 
 export const Item = styled.div`
@@ -79,6 +110,12 @@ export const Item = styled.div`
 `;
 
 export const Image = styled.img`
-
+    border: 0;
+    width: 100%;
+    max-width: 305px;
+    cursor: pointer;
+    height: auto;
+    padding: 0;
+    margin: 0;
 `;
 
