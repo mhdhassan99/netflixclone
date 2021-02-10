66e8f84d6afe37e8b540476f387dda127fddabc7
mhdhassan99 <mhassan.work99@gmail.com>
2021-02-09 22:45:50 -0500

    styling

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -40,23 +40,57 @@ export const Group = styled.div`
 `;
 
 export const SubTitle = styled.p`
-
+    font-size: 12px;
+    color: #fff;
+    font-weight: bold;
+    margin-top: 0;
+    margin-bottom: 0;
+    user-select: none;
+    display: none;
 `;
 
 export const Text = styled.p`
+    margin-top: 5px;
+    font-size: 10px;
+    color: #fff;
+    margin-bottom: 0;
+    user-select: none;
+    display: none;
+    line-height: normal;
+`;
 
+export const FeatureText = styled.p`
+    margin-left: 0;
 `;
 
 export const Feature = styled.div`
+    display: flex;
+    flex-direction: row;
+    background: url(${({ src }) => src});
+    background-size: contain;
+    position: relative;
+    height: 360px;
+    background-position-x: right;
+    background-repeat: no-repeat;
+    backgroundColor: black;
 
+    @media (max-width: 1000px) {
+        height: auto;
+        background-size: auto;
+
+        ${Title} {
+            font-size: 20px;
+            line-height: 20px;
+            margin-bottom: 10px;
+        }
+        ${FeatureText} {
+            font-size: 14px;
+        }
+    }
 `;
 
 export const FeatureTitle = styled(Title)`
     margin-left: 0;
-`;
-
-export const FeatureText = styled.p`
-    
 `;
 
 export const FeatureClose = styled.button`
@@ -68,10 +102,10 @@ export const FeatureClose = styled.button`
     background-color: transparent;
     border: 0;
 
-    img (
+    img {
         filter: brightness(0) invert(1)
         width: 24px;
-    )
+    }
 `;
 
 export const Maturity = styled.div`
@@ -85,11 +119,18 @@ export const Maturity = styled.div`
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
     margin-right: 10px;
     font-size: 12px;
-    
+
 `;
 
 export const Content = styled.div`
+    margin: 56px;
+    max-width: 500px;
+    line-height: normal;
 
+    @media (max-width: 1000px) {
+        margin: 30px;
+        max-width: none;
+    }
 `;
 
 export const Meta = styled.div`
@@ -106,7 +147,19 @@ export const Entities = styled.div`
 `;
 
 export const Item = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: 5px;
+    position: relative;
+    cursor: pointer;
+    transition: transform: 0.2s;
 
+    &:hover {
+        transform: scale(1.3);
+        z-index: 99;
+    }
+
+    
 `;
 
 export const Image = styled.img`
