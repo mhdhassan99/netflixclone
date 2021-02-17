817e5fa5404a504254e1b75d41b6a30fd0c817bd
mhdhassan99 <mhassan.work99@gmail.com>
2021-02-16 23:01:55 -0500

    styling player

--- src/components/player/styles/player.js
+++ src/components/player/styles/player.js
@@ -5,15 +5,55 @@ export const Container = styled.div`
 `;
 
 export const Overlay = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+    margin: 0 20px;
 `;
 
 export const Inner = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 900px;
+    margin: auto;
 
+    video {
+        height: 100%;
+        width: 100%
+    }
 `;
 
 export const Close = styled.button`
+    position: absolute;
+    right: 15px;
+    top: 15px;
+    width: 22px;
+    height: 22px;
+    opacity: 0.3;
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
 
+    &:hover {
+        opacity: 1;
+    }
+
+    &:before,
+    &:after {
+        position: absolute;
+        left: 10px;
+        top: 0;
+        content: ' ';
+        height: 22px;
+        width: 22px;
+        background-color: #333;  
+    }     
 `;
 
 export const Button = styled.button`
@@ -31,6 +71,10 @@ export const Button = styled.button`
     justify-content: center;
     align-items: center;
     padding-left: 0px;
-    
+
+    &:hover {
+        transform: scale(1.05);
+        background-color: #ff0a16;
+    }
 `;
 
