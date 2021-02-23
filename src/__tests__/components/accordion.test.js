e92a4c7e8b5adeed42899d97dcc8b9d44eda7c4c
mhdhassan99 <mhassan.work99@gmail.com>
2021-02-23 11:31:36 -0500

    accordion tests

---
+++ src/__tests__/components/accordion.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import faqsData from '../../fixtures/faqs.json';
+import { Accordion } from '../../components';
+
+describe('<Accordion />', () => {
+  it('renders the <Accordion /> with populated data', () => {
+    const { container, getByText } = render(
+      <Accordion>
+        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Frame>
+          {faqsData.map((item) => (
+            <Accordion.Item key={item.id}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body>{item.body}</Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion.Frame>
+      </Accordion>
+    );
+
+    expect(getByText('Frequently Asked Questions')).toBeTruthy();
+    expect(getByText('What is Netflix?')).toBeTruthy();
+    expect(getByText('How much does Netflix cost?')).toBeTruthy();
+    expect(getByText('Where can I watch?')).toBeTruthy();
+    expect(getByText('How do I cancel?')).toBeTruthy();
+    expect(getByText('What can I watch on Netflix?')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
