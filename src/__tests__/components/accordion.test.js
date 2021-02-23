64e232aa17257032209dad829a28f2ef4e56413a
mhdhassan99 <mhassan.work99@gmail.com>
2021-02-23 11:44:28 -0500

    working on card test

--- src/__tests__/components/accordion.test.js
+++ src/__tests__/components/accordion.test.js
@@ -7,7 +7,7 @@ describe('<Accordion />', () => {
   it('renders the <Accordion /> with populated data', () => {
     const { container, getByText } = render(
       <Accordion>
-        <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+        <Accordion.Title>Frequently asked questions</Accordion.Title>
         <Accordion.Frame>
           {faqsData.map((item) => (
             <Accordion.Item key={item.id}>
@@ -19,7 +19,7 @@ describe('<Accordion />', () => {
       </Accordion>
     );
 
-    expect(getByText('Frequently Asked Questions')).toBeTruthy();
+    expect(getByText('Frequently asked questions')).toBeTruthy();
     expect(getByText('What is Netflix?')).toBeTruthy();
     expect(getByText('How much does Netflix cost?')).toBeTruthy();
     expect(getByText('Where can I watch?')).toBeTruthy();
@@ -27,3 +27,31 @@ describe('<Accordion />', () => {
     expect(getByText('What can I watch on Netflix?')).toBeTruthy();
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it('opens and closes the <Accordion /> component', () => {
+    const { container, queryByText } = render (
+      <Accordion>
+        <Accordion.Title>Frequently asked questions</Accordion.Title>
+        <Accordion.Frame>
+          {faqsData.map((item) => (
+            <Accordion.Item key={item.id}>
+              <Accordion.Header>{item.header}</Accordion.Header>
+              <Accordion.Body data-testid="accordion-body">{item.body}</Accordion.Body>
+            </Accordion.Item>
+          ))}
+        </Accordion.Frame>
+      </Accordion>
+    );
+
+    const whatIsNetflixBodyText =
+      "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+
+    expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
+    fireEvent.click(queryByText('What is Netflix?'));
+    expect(queryByText(whatIsNetflixBodyText)).toBeTruthy();
+
+    fireEvent.click(queryByText('What is Netflix?'));
+    expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
