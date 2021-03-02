299dd69fe0dd58b4f32d91a03da82e8103f42d6a
mhdhassan99 <mhassan.work99@gmail.com>
2021-03-01 20:41:43 -0500

    wrote test for feature

--- src/__tests__/components/feature.test.js
+++ src/__tests__/components/feature.test.js
@@ -15,4 +15,28 @@ describe('<Feature />', () => {
         expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
         expect(container.firstChild).toMatchSnapshot();
     });
+
+    it('renders the <Feature /> with just a title', () => {
+        const { container, queryByText, getByText } = render(
+            <Feature>
+                <Feature.Title>Unlimited films, TV programmes and more</Feature.Title>
+            </Feature>
+        )
+
+        expect(getByText('Unlimited films, TV programmes and more.')).toBeTruthy();
+        expect(queryByText('Watch anywhere. Cancel at any time.')).toBeFalsy();
+        expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('renders the <Feature /> with just a subtitle', () => {
+        const { container, queryByText, getByText } = render(
+            <Feature>
+                <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+            </Feature>
+        )
+
+        expect(queryByText('Unlimited films, TV programmes and more.')).toBeFalsy();
+        expect(getByText('Watch anywhere. Cancel at any time.')).toBeTruthy();
+        expect(container.firstChild).toMatchSnapshot();
+    });
 })
